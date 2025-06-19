bfe8a3b42c02759e96bb1515d45f0bf1cf78c821
eyob-wendmagegn <eyobwende18@gmail.com>
2025-06-19 04:29:53 -0700

    update URI

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/EyDB')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
